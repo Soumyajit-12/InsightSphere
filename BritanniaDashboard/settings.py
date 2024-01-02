136810540e34ec19c9f4222466a59754ca58c690
Soumyajit-12 <soumyajit120503@gmail.com>
2024-01-02 12:54:41 +0530

    static changes

--- BritanniaDashboard/settings.py
+++ BritanniaDashboard/settings.py
@@ -112,6 +112,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'Interface/static')
 STATIC_URL = "static/"
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'Interface/static/InsightSphere'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
